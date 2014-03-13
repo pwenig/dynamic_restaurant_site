110da6ec396b3a3d4d28d4a53fc88c8375f50169
Paul Wenig <pwenig@gmail.com>
2014-03-13 14:09:29 -0600

    changes made along with mgehard. v1

---
+++ lib/item.rb
@@ -0,0 +1,20 @@
+require 'menu'
+
+class Item
+def initialize(name, price, description)
+  @name = name
+  @price = price
+  @description = description
+end
+  def name
+    @name
+  end
+
+  def price
+    @price
+  end
+
+  def description
+    @description
+  end
+end
