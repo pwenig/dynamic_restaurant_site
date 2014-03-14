6dd5c6369cc9b2a1885684c4121093e201089b46
Paul Wenig <pwenig@gmail.com>
2014-03-13 21:50:52 -0600

    matched  mikes version. refactored list as menu objects. items load from config file

---
+++ lib/menu.rb
@@ -0,0 +1,8 @@
+class Menu
+  def items
+    [
+    Item.new("Channa Masala", 5.95, "Yummy Goodness"),
+    Item.new("Chicken Tikka Masala", 5.95, "Yummy Goodness")
+    ]
+  end
+end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
+require 'item'
 
 describe 'Visiting the home page' do
-  
+
+
+
   it "displays all of the menu items" do
     visit "/"
     expect(page).to have_content("Channa Masala")
